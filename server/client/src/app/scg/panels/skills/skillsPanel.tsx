a12f164afd88a312282a409b3da53e671cd67d5c
Dominic Cousins <dc346@student.le.ac.uk>
2021-05-05 03:26:51 +0100

    Replaced default info buttons with styled ones. Added rules excerpt for  skills

--- server/client/src/app/scg/panels/skills/skillsPanel.tsx
+++ server/client/src/app/scg/panels/skills/skillsPanel.tsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import { SkillsPanelProps, SkillsPanelState } from "./skillsPanel.types"
 import "./skills.scss";
 import { CharacterContext, GameObjectContext } from "../../Scg.types";
-import PanelHeader from "../components/PanelHeader";
 import { Skill } from "../../../../types/object.types";
 import { Modal, Button } from "rsuite";
+import PanelFrame from "../panel/PanelFrame";
+import { skillsRulesExcerptLong, skillsRulesExcerptShort } from "./skillsDescription";
 
 /*
     Panel for choosing character skills
@@ -52,25 +53,24 @@ export default class SkillsPanel extends Component<SkillsPanelProps, SkillsPanel
         {
             const key = keys[i];
             out.push(
-                <tr key={key}>
-                    <td>
-                        <button className="button tiny"
-                            onClick={() => this.setState({ viewedSkill: skills.get(key), isInspecting: true })}
-                        >
-                            i
-                        </button>
-                    </td>
-                    <td>
+                <div key={key} className="skill-row">
+                    <Button className="button tiny skill-cell info-button"
+                        size="xs"
+                        onClick={() => this.setState({ viewedSkill: skills.get(key), isInspecting: true })}
+                    >
+                        i
+                    </Button>
+                    <p style={{minWidth: "82px"}} className="skill-cell">
                         {skills.get(key).name}
-                    </td>
-                    <td>
+                    </p>
+                    <p className="skill-cell">
                         {this.context.character.skills.earntSkills.get(skills.get(key).id)
                             ? this.context.character.skills.earntSkills.get(skills.get(key).id).level
                             : "-"
                         }
-                    </td>
-                    <td>
-                        <div style={{float: "left"}} className="button tiny">
+                    </p>
+                    <div className="flexbox skill-cell">
+                        <div className="button tiny">
                             { this.canMinus(skills.get(key)) &&
                             <button className="button tiny"
                                 onClick={() => this.context.operations.skills.removeBonusSkill(key)}
@@ -78,7 +78,7 @@ export default class SkillsPanel extends Component<SkillsPanelProps, SkillsPanel
                                 -
                             </button> }
                         </div>
-                        <div>
+                        <div className="button tiny">
                             { this.canPlus(skills.get(key)) &&
                             <button className="button tiny"
                                 onClick={() => this.context.operations.skills.learnBonusSkill(key)}
@@ -86,74 +86,76 @@ export default class SkillsPanel extends Component<SkillsPanelProps, SkillsPanel
                                 +
                             </button> }
                         </div>
-                    </td>
-                </tr>
+                    </div>
+                </div>
             );
         }
 
         // Wrap those in a table
         return (
-        <div className="flex grow">
-            <table className="skill-table">
-                <tbody>
+            <div className="skill-table flex grow">
                     {out}
-                </tbody>
-            </table>
-        </div>
+            </div>
         );
     }
 
     render() {
         return (
-        <GameObjectContext.Consumer>
-        { gameObjects => 
-            <div className="Skills Panel">
-                <PanelHeader
-                    onReset={this.context.operations.skills.resetSkills}
-                    onHelp={() => { /* TODO: help modal */ }}
-                />
-                <h1>Skills</h1>
-                <div>
-                    <h2 style={{marginBottom: 0, float: "left"}}>Available bonus skills:</h2>
-                    <h2 style={{marginBottom: 0, float: "right"}}>Skill points: {this.context.character.skills.skillPoints}</h2>
-                </div>
-                <div className="available-points">
-                    <div className="flex grow">
-                        <h3>
-                            {`${this.context.character.skills.availableBonuses.any} any skill`}
-                        </h3>
+        <PanelFrame
+            descriptionShort={skillsRulesExcerptShort}
+            descriptionLong={skillsRulesExcerptLong}
+            title="Skills"
+            className="Skills"
+        >
+            <GameObjectContext.Consumer>
+            { gameObjects => 
+                <>
+                    <div style={{display: "flex"}}>
+                        <h2 className="skill-cell">
+                            Skill points: {this.context.character.skills.skillPoints}
+                        </h2>
+                        <p className="skill-cell" style={{marginLeft: "auto", marginRight: "8px"}}>
+                            Costs 1 + current skill level to upgrade
+                        </p>
                     </div>
-                    <div className="flex grow">
-                        <h3>{`${this.context.character.skills.availableBonuses.combat} combat skill`}</h3>
+                    <div className="available-points">
+                        <div className="flex grow">
+                            <h3>
+                                {`${this.context.character.skills.availableBonuses.any} any skill`}
+                            </h3>
+                        </div>
+                        <div className="flex grow">
+                            <h3>{`${this.context.character.skills.availableBonuses.combat} combat skill`}</h3>
+                        </div>
+                        <div className="flex grow">
+                            <h3>{`${this.context.character.skills.availableBonuses.noncombat} non-combat skill`}</h3>
+                        </div>
                     </div>
-                    <div className="flex grow">
-                        <h3>{`${this.context.character.skills.availableBonuses.noncombat} non-combat skill`}</h3>
+                    <div className="flexbox">
+                        {this.makeSkillsTable(gameObjects.skills, 0, gameObjects.skills.size / 2)}
+                        {this.makeSkillsTable(gameObjects.skills, gameObjects.skills.size / 2)}
                     </div>
-                </div>
-                <div className="flexbox">
-                    {this.makeSkillsTable(gameObjects.skills, 0, gameObjects.skills.size / 2)}
-                    {this.makeSkillsTable(gameObjects.skills, gameObjects.skills.size / 2)}
-                </div>
-                <Modal show={this.state.isInspecting}
-                    onHide={() => this.setState({ isInspecting: false })}
-                >
-                    <Modal.Header>
-                        <Modal.Title>{ this.state.viewedSkill? this.state.viewedSkill.name : "error" }</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        {this.state.viewedSkill? this.state.viewedSkill.description : "error"}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={() => this.setState({ isInspecting: false })}
-                            appearance="primary"
-                        >
-                            OK
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        }
-        </GameObjectContext.Consumer>
+                </>
+            }
+            </GameObjectContext.Consumer>
+            <Modal show={this.state.isInspecting}
+                onHide={() => this.setState({ isInspecting: false })}
+            >
+                <Modal.Header>
+                    <Modal.Title>{ this.state.viewedSkill? this.state.viewedSkill.name : "error" }</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {this.state.viewedSkill? this.state.viewedSkill.description : "error"}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => this.setState({ isInspecting: false })}
+                        appearance="primary"
+                    >
+                        OK
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </PanelFrame>
         );
     }
 }
